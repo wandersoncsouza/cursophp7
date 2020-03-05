d9faf56d173250656016e3365b031133e815afd1
wandersoncsouza <w_costasouza@hotmail.com>
2020-03-05 14:20:34 -0300

    Create index.php
    Recreated project of article
    https://www.canalti.com.br/programacao/web/php/como-criar-sistema-de-login-com-php-e-mysql/

---
+++ outro/index.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de login - PHP + MySQL</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bulma.min.css">
+</head>
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+            
+            </div>
+        </div>
+    </section>
+</body>
