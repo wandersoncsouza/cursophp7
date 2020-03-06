05d5de25358cc451c21130c6375ebd43d4e79798
wandersoncsouza <w_costasouza@hotmail.com>
2020-03-05 23:21:21 -0300

    Created login.php

---
+++ outro/login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include('conexao.php');
+
+if(empty($_POST['usuario']) or empty($_POST['senha'])){
+  header('Locarion: index.php');
+  exit();
+}
+
+$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
+$query = "SELECT usuario FROM usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
+$result = mysqli_query($conexao, $query);
+$row = mysqli_num_rows($result);
+
+if($row == 1){
+  $_SESSION['usuario'] = $usuario;
+  header('Location: painel.php');
+  exit();
+}
+else {
+  $_SESSION['nao_autenticado'] = true;
+  header('Location: index.php');
+  exit();
+}
