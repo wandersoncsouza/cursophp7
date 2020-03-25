1ac45f2dfb6e07f9b99ec3e835cad82c2ac16122
wandersoncsouza <53883005+wandersoncsouza@users.noreply.github.com>
2020-03-25 19:42:53 -0300

    Curso PHP Poo -  aula 02 - Classes, métodos e objetos

--- phppoo_youtube/aula_02.php
+++ phppoo_youtube/aula_02.php
@@ -1,9 +1,17 @@
 <?php
-class Pessoa {
+class Pessoa {   //Criando uma classe
     public $nome;
     public $idade;
 
-    
+    //Criando um método
+    public function Falar(){
+    //    echo "Falou";
+    echo $this->nome." de ".$this->idade." anos, acabou de falar";
+    }   
 }
 
-?>
+$rodrigo = new Pessoa(); //Instanciar a classe
+$rodrigo->nome = "Rodrigo dos Santos de Oliveira";  //Fazendo a utilizaçao de um atributo
+$rodrigo->idade = 25; //Fazendo a utilizaçao de um atributo
+$rodrigo->Falar(); //Utilizando o método
+//echo $rodrigo->nome; // Exibindo valores do atributo - Isso só é possível devido o atributo nome é Public
