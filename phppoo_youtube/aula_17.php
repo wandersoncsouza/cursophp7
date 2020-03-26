64a8c0f7a590e90d33ee24c84fefc8eb2d9d69ea
wandersoncsouza <53883005+wandersoncsouza@users.noreply.github.com>
2020-03-26 20:57:05 -0300

    Curso PHP Poo - Aula 17 - Agregação

---
+++ phppoo_youtube/aula_17.php
@@ -0,0 +1,31 @@
+<?php
+
+class Produtos{
+    public $nome;
+    public $valor;
+
+    function __construct($nome, $valor){
+        $this->nome = $nome;
+        $this->valor = $valor;
+    }
+}
+class Carrinho {
+    public $produtos;
+    public function adiciona(Produtos $produto){
+        $this->produtos[] = $produto;
+    }
+    public function exibe(){
+        foreach($this->produtos as $produto){
+            echo $produto->nome.'<br>';
+            echo $produto->valor.'<hr>';
+        }
+    }
+}
+$produto1 = new Produtos("Notebook", "1500");
+$produto2 = new Produtos("Mouse", "30");
+
+$carrinho = new Carrinho();
+$carrinho->adiciona($produto1);
+$carrinho->adiciona($produto2);
+
+$carrinho->exibe();
